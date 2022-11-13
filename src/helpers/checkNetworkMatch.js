d5b1f1669943864d2bdc509855680e2ea4a0e245
FlashYield <86068494+FlashYield@users.noreply.github.com>
2022-11-13 15:00:25 +0530

    Update checkNetworkMatch.js

--- src/helpers/checkNetworkMatch.js
+++ src/helpers/checkNetworkMatch.js
@@ -3,7 +3,7 @@ import { bnbContractDetails, contractDetails } from "../constants";
 const checkNetworkMatch = async (payload) => {
   var t = await window.web3.eth.net.getId();
   var e = await window.web3.eth.getChainId();
-  if (56 === t && 56 === e) {
+  if (10 === t && 10 === e) {
     window.contract = new window.web3.eth.Contract(
       bnbContractDetails.abi,
       bnbContractDetails.address
@@ -12,7 +12,7 @@ const checkNetworkMatch = async (payload) => {
     payload.onSuccess();
     return;
   } else {
-    if (1 !== t || 1 !== e) {
+    if (42161 !== t || 42161 !== e) {
       payload?.onFailure && payload.onFailure("UNSUPPORTED_NETWORK");
       return;
     }
